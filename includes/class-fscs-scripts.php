cd047e708477ae3616ce0f680c10a4b27ae76b6e
Jethro Landa <jethro@rymera.com.au>
2023-10-26 03:21:36 +0800

    tweak

--- includes/class-fscs-scripts.php
+++ includes/class-fscs-scripts.php
@@ -48,8 +48,8 @@ class FSCS_Scripts
 
         // Backend Settings Page React App
         if(isset($_GET['page']) && $_GET['page'] == 'fuel_savings_settings'){
-            wp_enqueue_style('fuel-savings-setting-style', FSCS_PLUGIN_URL . '/js/settings/build/index.css');
-            wp_enqueue_script('fuel-savings-setting-script', FSCS_PLUGIN_URL . '/js/settings/build/index.js', array('wp-element', 'wp-i18n'), '1.0.0', true);
+            wp_enqueue_style('fuel-savings-setting-style', FSCS_JS_ROOT_URL . 'settings/build/index.css');
+            wp_enqueue_script('fuel-savings-setting-script', FSCS_JS_ROOT_URL . 'settings/build/index.js', array('wp-element', 'wp-i18n'), '1.0.0', true);
             wp_localize_script('fuel-savings-setting-script', 'fscs_settings', array(
                 'rest_url'   => esc_url_raw( get_rest_url() ),
                 'nonce' => wp_create_nonce( 'wp_rest' ),
