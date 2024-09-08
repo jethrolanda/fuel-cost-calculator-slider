9de0f8d64a9f312eac7420c9599bac59bc19cb68
Jethro Landa <jemaq_lan@hotmail.com>
2024-09-08 23:01:31 +0800

    build sass file

--- blocks/build/fuel_savings_calculator_slider/index.asset.php
+++ blocks/build/fuel_savings_calculator_slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f8c8e5936dcf627cebdf');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '40f6a16ae94ef246f100');
