28bd2b59a5684990b9daa57addce160e44ce27ba
Jethro Landa <jemaq_lan@hotmail.com>
2024-10-14 14:24:06 +0800

    styles update

--- blocks/build/fuel_savings_calculator_slider/index.asset.php
+++ blocks/build/fuel_savings_calculator_slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'c6df220b440dd25b6484');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '83606e3bac7b3140c22c');
