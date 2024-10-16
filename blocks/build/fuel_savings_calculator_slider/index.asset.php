a27c6db8aaa40317a4a938525cb8c0b5d90bbf72
Jethro Landa <jemaq_lan@hotmail.com>
2024-10-16 21:42:21 +0800

    add display block

--- blocks/build/fuel_savings_calculator_slider/index.asset.php
+++ blocks/build/fuel_savings_calculator_slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '83606e3bac7b3140c22c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'eeb117aeba298c84fd22');
